09206273269d818766ba1f377fbe99d668a8a231
akiizryzz <hayikuyee@gmail.com>
2025-06-02 16:22:39 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     const apiUrl = `https://www.furinnteam.web.id/ai/furina?content=${encodeURIComponent(content)}&user=${encodeURIComponent(user || 'user')}`
     const response = await fetch(apiUrl)
     const data = await response.json()
-    res.status(200).json(data)
+    res.status(200).json({ "status": true, "statusText": "OK", "statusCode": 200, "result": { "message": data.result.message }, "creator": "furinn.team" })
   } catch (error) {
     res.status(500).json({ status: false, creator: "@Kyzryzz", message: error.message })
   }
