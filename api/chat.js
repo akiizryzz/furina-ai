cdf6fd852969f5b2d92028d8c1d0a6b8c90f4733
akiizryzz <hayikuyee@gmail.com>
2025-06-02 16:24:12 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,11 +1,11 @@
 export default async function handler(req, res) {
-  if (req.method !== 'GET') {
-    res.status(405).json({ status: false, creator: "@Kyzryzz", message: 'Method Not Allowed' })
+  if (req.method !== 'GET' || req.method !== 'POST') {
+    res.status(405).json({ message: 'Method Not Allowed' })
     return
   }
   const { content, user } = req.query
   if (!content) {
-    res.status(400).json({ status: false, creator: "@Kyzryzz", message: 'Missing "content" parameter' })
+    res.status(400).json({ status: false, message: 'Missing "content" parameter' })
     return
   }
   try {
@@ -14,6 +14,6 @@ export default async function handler(req, res) {
     const data = await response.json()
     res.status(200).json({ "status": true, "statusText": "OK", "statusCode": 200, "result": { "message": data.result.message }, "creator": "furinn.team" })
   } catch (error) {
-    res.status(500).json({ status: false, creator: "@Kyzryzz", message: error.message })
+    res.status(500).json({ status: false, message: error.message })
   }
 }
